03cf08bdf1d27002db35ae1774627afd11ee18ba
AwadRoubahAhmed <awadroubahahmed@gmail.com>
2024-11-08 23:59:44 +0100

    useStat app commit!

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import Formulaire from "./components/Formulaire";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const [title, setNewTitle] = useState("Comprendre les states en Ract JS!");
+
+  const [number, setNumbers] = useState([1, 2, 3]);
+
+  const addBtnClick = () => {
+    setCount(count + 1);
+  };
+  const subBtnClick = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    } else {
+      alert("Counter cannot be negative!!");
+    }
+  };
+
+  const handelTitleChange = () => {
+    setNewTitle("React useState !!");
+
+    setNumbers([...number, 4]);
+  };
+
+  return (
+    <div className="App w-full h-svh bg-slate-900 text-white">
+      <h1 className="text-3xl text-center font-bold underline">{title}</h1>
+      <button
+        className=" bg-neutral-200 hover:bg-slate-400 text-black text-semibold text-base p-1.5 font-semibold rounded-md my-2.5"
+        onClick={handelTitleChange}
+      >
+        Change Title
+      </button>
+      <h4 className="text-lg mt-6 pl-3 ">Counter: {count}</h4>
+      <button
+        className=" bg-neutral-200 hover:bg-slate-400 text-black text-semibold text-base p-1.5 font-semibold rounded-md  ml-3 my-2.5"
+        onClick={addBtnClick}
+      >
+        Increments
+      </button>
+      <button
+        className=" bg-neutral-200 hover:bg-slate-400 text-black text-semibold text-base p-1.5 font-semibold rounded-md ml-3 my-2.5"
+        onClick={subBtnClick}
+      >
+        Decrements
+      </button>
+      <div className="box w-4/5 h-fit bg-neutral-300 text-black my-7 mx-auto rounded-md">
+        <h3 className="text-xl text-center underline font-semibold">
+          Les States en React
+        </h3>
+        <Formulaire />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Formulaire.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+
+function Formulaire() {
+  //Code JavaScript;
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+
+  const handelSubmit = (e) => {
+    e.preventDefault();
+
+    if (!isNaN(name) || isNaN(age)) {
+      alert("The champs Inputs is invalid!");
+    }
+
+    console.log(`Name: ${name}, Age: ${age}`);
+  };
+
+  //Code JSX;
+  return (
+    <div>
+      <form onSubmit={handelSubmit} className="w-1/2 h-fit my-5 mx-auto">
+        <div className="flex p-2">
+          <label className="pr-2">Name: </label>
+          <input
+            className="p-0.5 text-base capitalize"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Name"
+          />
+        </div>
+
+        <div className="flex p-2">
+          <label className="pr-2">Age: </label>
+          <input
+            className="p-0.5 text-sm"
+            type="text"
+            value={age}
+            onChange={(e) => setAge(e.target.value)}
+            placeholder="Age"
+          />
+        </div>
+        <p id="text" className="hidden text-red-500">
+          The champs is invalid!
+        </p>
+        <button
+          className=" bg-slate-800 hover:bg-slate-700 text-white text-base p-1.5 font-semibold rounded-md mx-auto my-2.5"
+          type="submit"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Formulaire;
